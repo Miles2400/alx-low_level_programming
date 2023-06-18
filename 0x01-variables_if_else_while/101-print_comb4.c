5ca629a75cbb7c229348f492ec3ceadbde21501c
root <root@MILES>
2023-06-18 22:58:53 +0100

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+	int z;
+
+	for (i = 48; i < 55; i++)
+		for (n = 49; n < 56; n++)
+			for (z = 50; z < 57; z++)
+			{
+				if ((i != n) && (i != z) && (n != z))
+				{
+					putchar(i);
+					putchar(n);
+					putchar(z);
+					putchar(',');
+					putchar(' ');
+				}
+				
+			}
+	putchar('\n');
+	return (0);
+}
+
