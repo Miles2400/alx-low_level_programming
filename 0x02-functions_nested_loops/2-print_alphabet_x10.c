e0834ae090c27c23c711175030b58df32377218d
Miles2400 <piusjay40@gmail.com>
2023-06-20 17:40:30 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int n = 0;
+
+	while (n <= 9)
+	{
+		for (i = 'a';i <= 'z';i++);
+	}
+	_putchar(1);
+	_putchar('\n');
+	n++;
+}
+
+
