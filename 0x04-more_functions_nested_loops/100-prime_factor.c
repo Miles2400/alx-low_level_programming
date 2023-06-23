95299674ed5e7b862e29afefe2b561283afbef08
Miles2400 <piusjay40@gmail.com>
2023-06-23 04:00:11 +0100

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,7 +3,8 @@
 
 /**
  * main - prints largest prime factor of 612852475143
- * Return:0
+ *
+ * Return: 0
  */
 
 int main(void)
@@ -12,8 +13,8 @@ int main(void)
 
 	for (i = 3; i < 782849;i = i + 2)
 	{
-			while ((n % i == 0) && (n != i))
-			n  = n / i;
+		while ((n % i == 0) && (n != i))
+		n  = n / i;
 	}
 	printf("%lu\n", n);
 	return (0);
