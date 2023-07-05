77b1410f63054ba402e9d4fc991e9ee5e5e2d8f9
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-07-06 00:04:43 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,14 +1,1 @@
-#include "main.h"
 
-/**
- * _print_rev_recursion - prints the given string in reverse
- * @s: string to be print printed
- */
-void _print_rev_recursion(char *s)
-{
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
-}
